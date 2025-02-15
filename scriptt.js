eabf36f138baea9865b10438e2d1600ae9779b4d
katyamen <katya1992arthur@gmail.com>
2025-02-15 16:53:04 -0300

    projeto

---
+++ scriptt.js
@@ -0,0 +1,19 @@
+const Titulo = document.getElementById("Titulo");
+const link = document.querySelector("a");
+const listaNaoOrdenada = document.querySelector("ul");
+const listaOrdenada = document.getElementById("lista-ordenada");
+Titulo.innerText ="Bem vindo ao projeto!";
+link.innerText ="Clique aqui para acessar o site da Prozeducação";
+
+listaNaoOrdenada.innerHTML =`
+<li>Amor</li>
+<li>programação</li>
+<li>aprender</li>`
+
+listaOrdenada.innerHTML =`
+<li><a href="https://www.google.com">Google</a></li>
+<li><a href="https://www.mozilla.org">Mozilla</a></li>
+<li><a href="https://www.github.com">Mozilla</a></li>`
+
+
+ 
